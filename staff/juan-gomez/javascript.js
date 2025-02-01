e4a36308b60057b4a957936389f9c5b46139fa14
unknown <juangomezhergo@gmail.com>
2025-02-01 19:08:08 +0100

    new changes in js file, but need more debbuging

--- staff/juan-gomez/javascript.js
+++ staff/juan-gomez/javascript.js
@@ -1,35 +1,28 @@
-//Crear una variable con el número ganador
-//Crear un bucle que recorra el código, con el límite establecido en que si se acierta el número, o el jugador no quiere jugar, el bucle se detiene. **Undefined en consola**
-//Crear una variable que sirva como pregunta para que el jugador decida (o no) jugar. Utilizar comando confirm para ello.
-//Crear un condicional en el que, si la decisión del jugador arroja True, el juego incia. 
-//Crear un mensaje de despedida si la condicional anterior arroja False.
-//Crear una variable para la elección del número que utiliará el jugador. En este caso, al tratarse de un número, se utilizara el comando prompt.
-//Crear un condicional, donde habrá un mensaje de felicitación si el jugador acierta el número ganador
-//Continuar generando condicionales, en base a los siguientes parámetros: "se ha quedado por debajo del número ganador" y "se ha quedado por encima del número ganador"
-// Definir comparativa número de selección jugador y número ganador
-// Definir variables por diferencia al alta o a la baja
-// Crear un bulce que recorra el código, con el límite que si se acierta el número, o el usuario no quiere jugar, se detiene
-
-var winNumber = 60
-
-while (playerChoice != winNumber && question != true) {
+var winNumber = Math.floor(Math.random() * 10) + 1
+while (parseInt(playerChoice) != winNumber && question != false) {
     var question = confirm('Do you want play a game?')
     if (question === true) {
         alert('Start game')
+        var playerChoice = parseInt(prompt('Choice your number'))
+        if (parseInt(playerChoice) === winNumber) {
+            alert('Congratulations, you win!')
+        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 2) {
+            alert('You loose, your number is a little bit small, you are very close to win')
+        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 5) {
+            alert('You loose, your number is small, you are close to win')
+        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) <= winNumber - 6) {
+            alert('You loose, your number is so small, you are far to win')
+        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 2) {
+            alert('You loose, your number is a little bit big, try with a smaller number, you are very close to win')
+        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 5) {
+            alert('You loose, your number is big, you are close tu win')
+        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) >= winNumber + 6) {
+            alert('You loose, your number is so big, you are far to win')
+        } else if (playerChoice != Number) {
+            alert('Are you sure you are introducings numbers???')
+        }
     } else {
         alert('Bye Bye')
-    }
-    var playerChoice = prompt('Choice your number')
-    if (playerChoice === winNumber.toString()) {
-        alert('Congratulations, you win!')
-    } if (playerChoice < winNumber.toString() && playerChoice === winNumber.toString() - 2) {
-        alert('You loose, the number is greater, just a little bit more')
-    } if (playerChoice < winNumber.toString() && playerChoice === winNumber.toString() - 5) {
-        alert('You loose, the number is greater, too cold')
-    } if (playerChoice < winNumber.toString() && playerChoice === winNumber.toString() + 2) {
-        alert('You loose, the number is lower, just a little bit less')
-    } if (playerChoice < winNumber.toString() && playerChoice === winNumber.toString() + 5) {
-        alert('You loose, the number is lower, to hot')
     }
 }
 
