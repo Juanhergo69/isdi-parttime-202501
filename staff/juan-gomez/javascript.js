79674e27b64128f3e6c8066272819ddb7da6e240
unknown <juangomezhergo@gmail.com>
2025-02-01 23:42:27 +0100

    little text modification

--- staff/juan-gomez/javascript.js
+++ staff/juan-gomez/javascript.js
@@ -2,7 +2,7 @@ var winNumber = Math.floor(Math.random() * 10) + 1
 while (parseInt(playerChoice) != winNumber && question != false) {
     var question = confirm('Do you want play a game?')
     if (question === true) {
-        alert('Start game')
+        alert('Welcome to "Guess the Number"')
         var playerChoice = parseInt(prompt('Choice your number'))
         if (parseInt(playerChoice) === winNumber) {
             alert('Congratulations, you win!')
@@ -13,7 +13,7 @@ while (parseInt(playerChoice) != winNumber && question != false) {
         } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) <= winNumber - 6) {
             alert('You loose, your number is so small, you are far to win')
         } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 2) {
-            alert('You loose, your number is a little bit big, try with a smaller number, you are very close to win')
+            alert('You loose, your number is a little bit big, you are very close to win')
         } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 5) {
             alert('You loose, your number is big, you are close tu win')
         } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) >= winNumber + 6) {
