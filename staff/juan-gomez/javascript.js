077574d070b56c1bec265a70fae3db7f6e4d1e3b
unknown <juangomezhergo@gmail.com>
2025-02-02 00:12:33 +0100

    last changes, code function is perfect

--- staff/juan-gomez/javascript.js
+++ staff/juan-gomez/javascript.js
@@ -6,15 +6,15 @@ while (parseInt(playerChoice) != winNumber && question != false) {
         var playerChoice = parseInt(prompt('Choice your number'))
         if (parseInt(playerChoice) === winNumber) {
             alert('Congratulations, you win!')
-        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 2) {
+        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 2 || parseInt(playerChoice) === winNumber - 1) {
             alert('You loose, your number is a little bit small, you are very close to win')
-        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 5) {
+        } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) === winNumber - 5 || parseInt(playerChoice) === winNumber - 4 || parseInt(playerChoice) === winNumber - 3) {
             alert('You loose, your number is small, you are close to win')
         } else if (parseInt(playerChoice) < winNumber && parseInt(playerChoice) <= winNumber - 6) {
             alert('You loose, your number is so small, you are far to win')
-        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 2) {
+        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 2 || parseInt(playerChoice) === winNumber + 1) {
             alert('You loose, your number is a little bit big, you are very close to win')
-        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 5) {
+        } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) === winNumber + 5 || parseInt(playerChoice) === winNumber + 4 || parseInt(playerChoice) === winNumber + 3) {
             alert('You loose, your number is big, you are close tu win')
         } else if (parseInt(playerChoice) > winNumber && parseInt(playerChoice) >= winNumber + 6) {
             alert('You loose, your number is so big, you are far to win')
