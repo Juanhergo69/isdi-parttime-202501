832569a42b3f858499c1a65dbc24b5b76d1bd21c
unknown <juangomezhergo@gmail.com>
2025-02-06 18:33:10 +0100

    add little changes in guesTheNumber.js and index.html #2

--- staff/juan-gomez/Files/guessTheNumber.js
+++ staff/juan-gomez/Files/guessTheNumber.js
@@ -1,49 +1,46 @@
 var winNumber = Math.floor(Math.random() * 10) + 1
 var failedAttemps = 0
 var question = confirm('Do you want play a game?')
+var playerChoice
 
-if (question === true) {
+if (question) {
     alert('Welcome to "Guess the Number"')
 } else {
     alert('Bye, Bye')
 }
 
 while (Number(playerChoice) !== winNumber && question !== false && failedAttemps < 3) {
-    var playerChoice = (prompt('Choice your number'))
+    playerChoice = (prompt('Choice your number'))
     if (playerChoice === null) {
         alert('You close the game, come back anytime :D')
         break
     }
-    if (isNaN(Number(playerChoice)) || playerChoice.trim() === '') {
+    if (isNaN(playerChoice) || playerChoice.trim() === '') {
+        failedAttemps++
         alert('Are you sure you are introducings numbers???')
     } else {
         if (Number(playerChoice) === winNumber) {
-            alert('Congratulations, you win!')
-        } else if ((Number(playerChoice) < winNumber) && (Number(playerChoice) === winNumber - 2
-            || Number(playerChoice) === winNumber - 1)) {
-            alert('You loose, your number is a little bit small, you are very close to win')
-        } else if ((Number(playerChoice) < winNumber) && (Number(playerChoice) === winNumber - 5
-            || Number(playerChoice) === winNumber - 4 || Number(playerChoice) === winNumber - 3)) {
-            alert('You loose, your number is small, you are close to win')
-        } else if (Number(playerChoice) < winNumber && Number(playerChoice) <= winNumber - 6) {
-            alert('You loose, your number is so small, you are far to win')
-        } else if ((Number(playerChoice) > winNumber) && (Number(playerChoice) === winNumber + 2 ||
-            Number(playerChoice) === winNumber + 1)) {
-            alert('You loose, your number is a little bit big, you are very close to win')
-        } else if ((Number(playerChoice) > winNumber) && (Number(playerChoice) === winNumber + 5 ||
-            Number(playerChoice) === winNumber + 4 || Number(playerChoice)) === winNumber + 3) {
-            alert('You loose, your number is big, you are close tu win')
-        } else if (Number(playerChoice) > winNumber && Number(playerChoice) >= winNumber + 6) {
-            alert('You loose, your number is so big, you are far to win')
+            alert('Congratulations, you win! Total failed attemps:' + failedAttemps)
+        } else if (Number(playerChoice) === winNumber - 1 || Number(playerChoice) === winNumber - 2) {
+            alert('You lose, your number is a little bit small, you are very close to win')
+        } else if (Number(playerChoice) === winNumber - 3 || Number(playerChoice) === winNumber - 4
+            || Number(playerChoice) === winNumber - 5) {
+            alert('You lose, your number is small, you are close to win')
+        } else if (Number(playerChoice) <= winNumber - 6) {
+            alert('You lose, your number is so small, you are far to win')
+        } else if (Number(playerChoice) === winNumber + 1
+            || Number(playerChoice) === + 2) {
+            alert('You lose, your number is a little bit big, you are very close to win')
+        } else if (Number(playerChoice) === winNumber + 3 || Number(playerChoice) === + 4
+            || Number(playerChoice) === + 5) {
+            alert('You lose, your number is big, you are close tu win')
+        } else if (Number(playerChoice) >= winNumber + 6) {
+            alert('You lose, your number is so big, you are far to win')
         }
-    }
-    if (isNaN(Number(playerChoice)) || Number(playerChoice) !== winNumber || playerChoice.trim() === '') {
         failedAttemps++
-    } if (failedAttemps === 3) {
-        alert('You loose the game, try again if you dare')
     }
-    if (Number(playerChoice) === winNumber || failedAttemps === 3) {
-        alert("failedAttemps:" + failedAttemps)
+    if (failedAttemps === 3) {
+        alert('You lost the game, try again if you dare. Total failed attemps:' + failedAttemps)
     }
 }
 
@@ -51,9 +48,4 @@ while (Number(playerChoice) !== winNumber && question !== false && failedAttemps
 
 
 
-
-/*Tengo dos errores que detecto:
-- Cuando el jugador clica en aceptar sin haber ingresado ningún número, lo cuenta como error.
-- Cuando se gana la partida, no cuenta los errores cometidos
-*/
 
