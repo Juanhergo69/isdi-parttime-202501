ee65d4c04e026e1ba67451aefef8b947388ab86c
unknown <juangomezhergo@gmail.com>
2025-02-16 22:04:59 +0100

    add new piedrapapeltijera.js file, with Janai's work added #2

--- staff/juan-gomez/Files/challenge/piedrapapeltijera.js
+++ staff/juan-gomez/Files/challenge/piedrapapeltijera.js
@@ -6,16 +6,22 @@ function compareChoices(_choice) { //Creamos función para comparar la elección
         alert('Empate!')
     } else if (_choice === 'Piedra' && cpuChoices === 'Tijera') {
         alert('Ganas!')
+
     } else if (_choice === 'Piedra' && cpuChoices === 'Papel') {
         alert('Pierdes!')
+
     } else if (_choice === 'Papel' && cpuChoices === 'Piedra') {
         alert('Ganas!')
+
     } else if (_choice === 'Papel' && cpuChoices === 'Tijera') {
         alert('Pierdes!')
+
     } else if (_choice === 'Tijera' && cpuChoices === 'Papel') {
         alert('Ganas!')
+
     } else if (_choice === 'Tijera' && cpuChoices === 'Piedra') {
         alert('Pierdes!')
+
     }
 }
 
@@ -24,12 +30,38 @@ var body = document.body; //Creamos el body//
 body.style.display = 'flex'; //Le damos estilos//
 body.style.flexDirection = 'column'; //Más estilos//
 body.style.alignItems = 'center'; //Más estilos//
+body.style.height = '500px'; //sino no llegaba a coger las boxes de los choices
+
+var link = document.createElement("link");
+link.href = "https://fonts.googleapis.com/css2?family=Miniver&display=swap";
+link.rel = "stylesheet";
+document.head.appendChild(link) //he creado una typo nueva
 
 var gameTitle = document.createElement('h1'); //Creamos título del juego//
 gameTitle.textContent = 'Piedra, Papel o Tijera'; //Damos un nombre al título del juego//
 gameTitle.style.textAlign = 'center'; //Le damos estilos//
+gameTitle.style.position = 'absolute'; //Le damos posición//
+gameTitle.style.fontFamily = "Miniver"; //aplico la typo nueva
+gameTitle.style.fontSize = '40px'; //Le damos un tamaño de fuente al texto//
+gameTitle.style.letterSpacing = "2px"; //Le damos un espaciado//
+gameTitle.style.color = "black"; //Le damos un color al texto//
+gameTitle.style.padding = '8px';
+gameTitle.style.textShadow = '10px 10px 20px rgba(0, 0, 0, 0.5)';
 
-body.appendChild(gameTitle); //Añadimos el título del juego al body//
+
+var gameTitleBox = document.createElement('div');
+gameTitleBox.style.width = '420px';
+gameTitleBox.style.height = '100px';
+gameTitleBox.style.backgroundColor = 'beige';
+gameTitleBox.style.position = 'relative';
+gameTitleBox.style.border = '1px solid black';
+gameTitleBox.style.padding = '10px'; //defino el espacio del texto respecto al borde de la caja
+gameTitleBox.style.textAlign = 'center';
+gameTitleBox.style.marginTop = '10px';
+gameTitleBox.style.boxShadow = '10px 10px 20px rgba(0, 0, 0, 0.5)'; //unas sombritas para la caja del titulo
+
+gameTitleBox.appendChild(gameTitle); //Añadimos el título del juego al div de la caja del titulo
+body.appendChild(gameTitleBox); //añadimos caja con titulo al body
 
 var tijeraimg = document.createElement('img') //Se añade imagen para tijera//
 tijeraimg.src = 'https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/123.png' //Ruta de la imagen//
@@ -55,43 +87,92 @@ papelimg.style.marginRight = '0px'; //Añadimos estilos//
 
 body.appendChild(papelimg) //Añadimos al body la imagen papel//
 
+var backgroundImage = document.createElement('div'); //lo he puesto en un div porque era todo un div para el background pero no se si es la mejor manera
+backgroundImage.style.backgroundImage = `url('https://wallpapercave.com/wp/wp3961352.png')`;
+backgroundImage.style.position = 'fixed';
+backgroundImage.style.backgroundPosition = 'center';
+backgroundImage.style.backgroundSize = 'cover'; //hace que cubra el fondo ampliando la imagen
+backgroundImage.style.height = '100vh'; //ajusta al total del alto
+backgroundImage.style.width = '100vw'; //ajusta al total del ancho
+backgroundImage.style.zIndex = '-1'; //pone el fondo detras del contenido
+backgroundImage.style.top = '0'; //sino se quedaba un margen pequeño arriba
+backgroundImage.style.filter = 'blur(2px)'; //le he añadido blur para que contrastarann mejor los elementos de delante y no tener que hacer cajitas o contornos
+
+body.appendChild(backgroundImage); //añadimos la imagen de fondo al body
 
 function renderUserChoice(_choice) { //La función renderiza en pantalla la selección hecha por el usuario//
+    userChoiceBox.innerHTML = ''; //borra contenido anterior
     var userChoice = document.createElement('div') //Se crea el mensaje//
     userChoice.textContent = `Has elegido: ${_choice}` //Se añade lo que queremos que diga el mensaje//
     userChoice.style.fontSize = '25px'; //Añadimos estilos//
     userChoice.style.height = '100px'; //Añadimos estilos//
     userChoice.style.width = '300px'; //Añadimos estilos//
-    userChoice.style.position = 'absolute' //Añadimos estilos//
+    userChoice.style.position = 'relative' //Añadimos estilos//
     userChoice.style.left = '200px'; //Añadimos estilos//
     userChoice.style.top = '200px'; //Añadimos estilos//
 
-    body.appendChild(userChoice) //Añadimos al body el mensaje creado//
+    userChoiceBox.appendChild(userChoice) //Añadimos al body el mensaje creado//
 }
 
 function renderCpuChoice(_cpuChoice) { //La función renderiza en pantalla la selección hecha por la CPU//
+    cpuChoiceBox.innerHTML = ''; //borra contenido anterior
     var cpuChoice = document.createElement('div') //Se crea el mensaje//
     cpuChoice.textContent = `La CPU ha elegido: ${_cpuChoice}` //Se añade lo que queremos que diga el mensaje//
     cpuChoice.style.fontSize = '25px'; //Añadimos estilos//
     cpuChoice.style.height = '100px'; //Añadimos estilos//
     cpuChoice.style.width = '300px'; //Añadimos estilos//
-    cpuChoice.style.position = 'absolute' //Añadimos estilos//
+    cpuChoice.style.position = 'relative' //Añadimos estilos//
     cpuChoice.style.left = '1500px'; //Añadimos estilos//
     cpuChoice.style.top = '200px'; //Añadimos estilos//
+    cpuChoice.style.color = 'black';
+    cpuChoice.style.fontFamily = 'Miniver';
 
-    body.appendChild(cpuChoice) //Añadimos al body el mensaje creado//
+    cpuChoiceBox.appendChild(cpuChoice) //Añadimos al body el mensaje creado//
 }
 
+var userChoiceBox = document.createElement('div');
+userChoiceBox.style.width = '350px';
+userChoiceBox.style.height = '50px';
+userChoiceBox.style.zIndex = '-1';
+userChoiceBox.style.backgroundColor = 'beige';
+userChoiceBox.style.padding = '8px';
+userChoiceBox.style.borderColor = 'black';
+userChoiceBox.style.border = '1px solid black';
+userChoiceBox.style.borderRadius = '20px'; //redondeo los bordes de la caja
+userChoiceBox.style.boxShadow = '10px 10px 20px rgba(0, 0, 0, 0.5)'; //le añado sombra a la caja
+userChoiceBox.style.position = 'absolute';
+userChoiceBox.style.left = '100px';
+userChoiceBox.style.top = '180px';
+
+var cpuChoiceBox = document.createElement('div');
+cpuChoiceBox.style.width = '350px';
+cpuChoiceBox.style.height = '50px';
+cpuChoiceBox.style.zIndex = '-1';
+cpuChoiceBox.style.backgroundColor = 'beige';
+cpuChoiceBox.style.padding = '8px';
+cpuChoiceBox.style.borderColor = 'black';
+cpuChoiceBox.style.border = '1px solid black';
+cpuChoiceBox.style.borderRadius = '20px'; //redondeo los bordes de la caja
+cpuChoiceBox.style.boxShadow = '10px 10px 20px rgba(0, 0, 0, 0.5)'; //le añado sombra a la caja
+cpuChoiceBox.style.position = 'absolute';
+cpuChoiceBox.style.right = '100px';
+cpuChoiceBox.style.top = '180px';
+
+body.appendChild(cpuChoiceBox);
+body.appendChild(userChoiceBox);
+
+//CREO QUE LA HE LIADO PORQUE NO ME APARECEN LOS MENSAJES DE CPU HA ELEGIDO 
 
 var buttonContainer = document.createElement('div') //Creamos contenedor de botones para selección del jugador//
 
 function choiceButtonContainer() { //La función genera el contenedor de botones y las posibles elecciones del jugador. Esto variará si cambiamos los elementos del array choices//
     buttonContainer.style.display = 'flex'; //Le damos estilos al contenedor de botones//
-    buttonContainer.style.width = '100%'; //Más estilos//
+    buttonContainer.style.width = '30%'; //Más estilos//
     buttonContainer.style.justifyContent = 'center'; //Más estilos//
     buttonContainer.style.gap = '2rem'; //Más estilos//
     buttonContainer.style.position = 'absolute'; //Más estilos//
     buttonContainer.style.top = '400px'; //Más estilos//
+    buttonContainer.style.borderRadius = '20px'; //redondeo los bordes de la caja
 
     for (var i = 0; i < choices.length; i++) { //El for itera sobre los elementos del array choices//
         generateChoiceButton(choices[i]) //Y a continuación ejecuta la función para generar los botones de selección en pantalla//
@@ -103,7 +184,7 @@ function choiceButtonContainer() { //La función genera el contenedor de botones
 function generateChoiceButton(_choice) { //La función genera el botón de selección del jugador//
     var button = document.createElement('button'); //Creamos el botón de selección//
     button.textContent = _choice; //Indicamos que el texto que incluirá serán las elecciones englobadas en el array choices//
-    button.style.fontSize = '25px';
+    button.style.fontSize = '25px'; //Damos un un tamaño de fuente determinado al botón//
     button.style.height = '50px'; //Damos un altura determinada al botón//
     button.style.width = '600px'; //Damos una anchura determinada al botón//
     button.style.borderColor = 'limegreen' //Damos un color al borde del botón//
